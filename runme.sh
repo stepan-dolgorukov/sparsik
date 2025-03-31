8d474f57818cf9bb0bbae3227a773a58d24b2f43
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-03-31 08:55:25 +0500

    Информирование о неуспехах; выполнение команды только при успешном выполнении предыдущей команды

--- runme.sh
+++ runme.sh
@@ -2,12 +2,24 @@
 
 rm --force ./A ./A.gz ./B ./B.gz ./C ./D
 make
+
+if [ "${?}" -ne 0 ]; then
+  printf "Unsuccessfull build the program \"sparsik\".\n"
+  exit 1
+fi
+
 chmod u+x ./create_A.py
 ./create_A.py
-./sparsik ./A ./B
-gzip --keep ./A
-gzip --keep ./B
-gzip --stdout --decompress ./B.gz | ./sparsik ./C
-./create_A.py
-./sparsik -s 100 ./A ./D
+
+if [ ! -f "./A" ]; then
+  printf "Unsuccessfull create of the file \"A\", it doesn't exist.\n"
+  exit 1
+fi
+
+./sparsik ./A ./B && \
+gzip --keep ./A && \
+gzip --keep ./B && \
+gzip --stdout --decompress ./B.gz | ./sparsik ./C && \
+./create_A.py && \
+./sparsik -s 100 ./A ./D && \
 stat --format='File name = %n, number of blocks allocated = %b' ./A ./A.gz ./B ./B.gz ./C ./D
